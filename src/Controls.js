3162988aa53f0c606a2a60ef421f053adf379f48
Andrew <63925540+An-drew-bit@users.noreply.github.com>
2021-12-24 12:51:09 +0300

    Update Controls.js

--- src/Controls.js
+++ src/Controls.js
@@ -6,17 +6,12 @@ class Controls {
         this.pauseBtnEl = document.getElementById('pauseBtn');
     }
 
-    /**
-     * @param {Game} game
-     */
+    
     init(game) {
         this.game = game;
     }
 
-    /**
-     * Метод устанавливает обработчики событий на клики по кнопкам
-     * "Старт" и "Пауза", а так же на стрелки перемещения змейки.
-     */
+   
     addControlsEventListeners() {
         this.startBtnEl.addEventListener('click', this.game.start.bind(this.game));
         this.pauseBtnEl.addEventListener('click', this.game.pause.bind(this.game));
