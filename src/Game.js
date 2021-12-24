54e9fef200616b53178fed19387cffc3ab043a10
Andrew <63925540+An-drew-bit@users.noreply.github.com>
2021-12-24 12:52:03 +0300

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -6,16 +6,7 @@ class Game {
         this.messageEl = document.getElementById('message');
     }
 
-    /**
-     * Метод получает другие игровые обьекты, которые нужны ему 
-     * для работы.
-     * @param {Settings} settings 
-     * @param {Status} status 
-     * @param {Board} board 
-     * @param {Snake} snake 
-     * @param {Menu} menu 
-     * @param {Score} score
-     */
+    
     init(settings, status, board, snake, controls, score) {
         this.settings = settings;
         this.status = status;
@@ -25,9 +16,7 @@ class Game {
         this.score = score;
     }
 
-    /**
-     * Метод запускает игру.
-     */
+    
     start() {
         if (this.status.isPaused()) {
             this.status.setPlaying();
@@ -35,23 +24,14 @@ class Game {
         }
     }
 
-    /**
-     * Метод ставит игру на паузу.
-     */
+    
     pause() {
         if (this.status.isPlaying()) {
             this.status.setPaused();
             this.stopGame();
         }
     }
-
-    /**
-     * Этот метод запускается каждую секунду и осуществляет:
-     * 1. перемещение змейки
-     * 2. проверяет проиграна/выиграна ли игра
-     * 3. увеличивает размер змейки если она ест еду
-     * 4. заново отрисовывает положение змейки и еды
-     */
+    
     doTick() {
         this.snake.performStep();
         if (this.isSnakeSteppedOntoItself()) {
@@ -76,19 +56,12 @@ class Game {
         this.board.renderSnake();
     }
 
-    /**
-     * Метод проверяет выиграна ли игра.
-     * @returns {boolean} если длина змейки достигла длины нужной 
-     * для выиграша, тогда true, иначе false.
-     */
+    
     isGameWon() {
         return this.snake.body.length == this.settings.winLength;
     }
 
-    /**
-     * Метод проверяет сьела ли змейка сама себя.
-     * @returns {boolean}
-     */
+   
     isSnakeSteppedOntoItself() {
         let cellArr = this.snake.body.map(function (cellCoords) {
             return cellCoords.x.toString() + cellCoords.y.toString();
@@ -101,11 +74,7 @@ class Game {
         clearInterval(this.tickIdentifier);
     }
 
-    /**
-     * В зависимости от нажатой кнопки (вверх, вниз, влево, вправо) будет
-     * вызываться соответствующий метод.
-     * @param {KeyboardEvent} event
-     */
+    
     pressKeyHandler(event) {
         switch (event.key) {
             case "ArrowUp":
@@ -123,10 +92,6 @@ class Game {
         }
     }
 
-    /**
-     * Метод выводит сообщение на странице.
-     * @param {string} text
-     */
     setMessage(text) {
         this.messageEl.innerText = text;
     }
