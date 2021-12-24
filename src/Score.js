89109e9e5e09ae44e688fe57ad498ab67e045770
Andrew <63925540+An-drew-bit@users.noreply.github.com>
2021-12-24 12:58:43 +0300

    Update Score.js

--- src/Score.js
+++ src/Score.js
@@ -6,26 +6,16 @@ class Score {
         this.toWinEl = document.querySelector('.toWin');
     }
 
-    /**
-     * @param {Settings} settings настройки игры.
-     */
+    
     init(settings) {
         this.settings = settings;
     }
 
-    /**
-     * Метод устанавливает текущий счет игрока.
-     * @param {string} score
-     */
+    
     renderCurrentScore(score) {
         this.currentEl.textContent = score;
     }
 
-    /**
-     * Метод устанавливает количество очков, необходимых
-     * для выигрыша.
-     * @param {string} points
-     */
     renderPointsForWin(points) {
         this.toWinEl.textContent = points;
     }
